189bcc508c4ccecbc3b09d039da306641de8af8f
Alex Palma <alex-palma@outlook.com>
2018-03-22 00:54:29 +1100

    Add rspec test for get_readable_route RouteFinder method

--- spec/route_finder_spec.rb
+++ spec/route_finder_spec.rb
@@ -1,2 +1,11 @@
 require 'spec_helper'
 
+describe RouteFinder do
+  subject { described_class.new(nil) }
+  describe 'get_readable_route' do
+    it 'returns a string of all the string elements of the array
+    joined together by ~> symbols' do
+      expect(subject.send :get_readable_route, ['test','this']).to eq 'test ~> this'
+    end
+  end
+end
